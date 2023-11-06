c76eaf8b3a4291ebe8ff46cdefe5b459bc0eedbe
unknown <ashuisalive@gmail.com>
2023-11-06 13:25:10 +0530

    details card completed

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import data from "../data/DetailsData.json";
+import DetailsCard from './DetailsCard';
+import { v4 as uuidv4 } from 'uuid';
+
+// const url = 'https://costa-data.s3.amazonaws.com/data/DetailsData.json;'
 
 const Details = () => {
-  console.log("data", data);
   return (
-    <div>Details</div>
+    <div>
+      {data?.map((item) => <DetailsCard key={uuidv4()} {...item} />)}
+    </div>
   )
 }
 
